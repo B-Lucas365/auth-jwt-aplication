fe49c677ff1551d3e58d9ce330540ecdfab26472
Lucas Renan <lucasrenan365@outlook.com>
2023-07-19 22:58:49 -0300

    applying refresh token

--- jwt-login/src/services/api.ts
+++ jwt-login/src/services/api.ts
@@ -1,11 +1,49 @@
-import axios from "axios";
-import {parseCookies} from 'nookies'
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies()
+let cookies = parseCookies();
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    headers: {
-        Authorization: `Bearer ${cookies['nextauth.token']}`
+  baseURL: "http://localhost:3333",
+  headers: {
+    Authorization: `Bearer ${cookies["nextauth.token"]}`,
+  },
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  
+  (error) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "nextauth.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken
+          }).then((response) => {
+            const { token } = response.data;
+
+            setCookie(undefined, "nextauth.token", token, {
+              maxAge: 60 * 60 * 24,
+              path: "/",
+            });
+
+            setCookie(undefined, "nextauth.refreshToken", response.data.refreshToken, {
+              maxAge: 60 * 60 * 24,
+              path: "/",
+            });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+          });
+      } else {
+        //deslogar o usuário
+      }
     }
-})
+  }
+);
